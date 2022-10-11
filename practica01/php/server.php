7eca696fda220d5915c4e8d2d4eff63942d8079b
pswsm <pauseawesome@protonmail.com>
2022-10-11 16:16:36 +0200

    User constant

--- practica01/php/server.php
+++ practica01/php/server.php
@@ -2,7 +2,7 @@
 define("USERS", ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
 $data = file_get_contents("php://input");
 $parsedData = json_decode($data, true);
-if (array_key_exists($parsedData["username"], $USERS)) {
+if (array_key_exists($parsedData["username"], USERS)) {
 	$ret_value = json_encode([$USERS[$parsedData["username"]]]);
 	echo $ret_value;
 };
