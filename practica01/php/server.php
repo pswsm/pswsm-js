c6d4f96a7824730d5623344e77ab606b908e0d97
pswsm <pauseawesome@protonmail.com>
2022-10-11 16:02:47 +0200

    Add var_dump(arr) to see how it works

--- practica01/php/server.php
+++ practica01/php/server.php
@@ -1,5 +1,6 @@
 <?php
+define($USERS, ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
 $data = file_get_contents("php://input");
 $parsedData = json_decode($data);
-echo "Done";
+var_dump($parsedData);
 ?>
