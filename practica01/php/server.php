b5ee5b4dc6133d3250cfb3ae193dc30017fbaacd
pswsm <pauseawesome@protonmail.com>
2022-10-11 16:15:36 +0200

    Decode as associative array

--- practica01/php/server.php
+++ practica01/php/server.php
@@ -1,7 +1,7 @@
 <?php
 define("USERS", ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
 $data = file_get_contents("php://input");
-$parsedData = json_decode($data);
+$parsedData = json_decode($data, true);
 if (array_key_exists($parsedData["username"], $USERS)) {
 	$ret_value = json_encode([$USERS[$parsedData["username"]]]);
 	echo $ret_value;
