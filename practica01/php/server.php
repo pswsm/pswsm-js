07c9791e4716b5d816e26935e8e040ad65f5e56e
pswsm <pauseawesome@protonmail.com>
2022-10-11 16:11:10 +0200

    User array creation

--- practica01/php/server.php
+++ practica01/php/server.php
@@ -1,6 +1,9 @@
 <?php
-define($USERS, ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
+define("USERS", ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
 $data = file_get_contents("php://input");
 $parsedData = json_decode($data);
-var_dump($parsedData);
+if (array_key_exists($parsedData["username"], $USERS)) {
+	$ret_value = json_encode([$USERS[$parsedData["username"]]]);
+	return $ret_value;
+};
 ?>
