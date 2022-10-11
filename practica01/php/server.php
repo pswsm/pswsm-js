22fe50507451b0caebdd0a2a54bcbd8786331026
pswsm <pauseawesome@protonmail.com>
2022-10-11 16:26:06 +0200

    Handle all situations relating the login form

--- practica01/php/server.php
+++ practica01/php/server.php
@@ -1,9 +1,15 @@
 <?php
-define("USERS", ["pswsm" => ["pswsm-adm", "administrador"], "notpswsm" => ["pswsm-clt", "client"]]);
+define("USERS", ["pswsm" => ["password" => "pswsm-adm", "role" => "administrador"], "notpswsm" => ["password" => "pswsm-clt", "role" => "client"]]);
 $data = file_get_contents("php://input");
 $parsedData = json_decode($data, true);
 if (array_key_exists($parsedData["username"], USERS)) {
-	$ret_value = json_encode([$parsedData["username"], USERS[$parsedData["username"]][1]]);
-	echo $ret_value;
+	if (USERS[$parsedData["username"]["password"]] == $parsedData["password"]) {
+		$ret_value = json_encode([$parsedData["username"], USERS[$parsedData["username"]][1]]);
+	} else {
+		$ret_value = json_encode("Password does not match");
+	}
+} else {
+	$ret_value = json_encode("Username not found");
 };
+echo $ret_value;
 ?>
