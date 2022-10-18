622fca8755f050fb12bb28c0196b7031f059798c
pswsm <pauseawesome@protonmail.com>
2022-10-19 00:53:25 +0200

    Move db to csv (very safe). User creation give 500 [WIP]

--- practica01/js/index.js
+++ practica01/js/index.js
@@ -4,12 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
 		xhrReq.open("POST", "./php/server.php")
 		xhrReq.send(JSON.stringify(
 			{
+				"operation": "login",
 				"username": document.getElementById("username_login").value,
 				"password": document.getElementById("password_login").value
 			})
 		)
 		xhrReq.onload = function() {
+			//console.log('xhrReq:', xhrReq);
 			document.getElementById("response").innerHTML = JSON.parse(xhrReq.response);
+		}
+	})
+	document.getElementById("submit_register").addEventListener("click", function () {
+		if (document.getElementById("password_login_1").value === document.getElementById("password_login_2").value) {
+			let xhrReq = new XMLHttpRequest()
+			xhrReq.open("POST", "./php/server.php")
+			xhrReq.send(JSON.stringify(
+				{
+					"operation": "create",
+					"username": document.getElementById("username_register").value,
+					"password": document.getElementById("password_login_1").value
+				})
+			)
+
+			xhrReq.onload = function() {
+				console.log('xhrReq:', xhrReq);
+				// document.getElementById("response").innerHTML = JSON.parse(xhrReq.response);
+			}
+		} else {
+			document.getElementById("response").innerHTML = "Passwords must match!"
 		}
 	})
 })
--- practica01/php/server.php
+++ practica01/php/server.php
@@ -1,15 +1,59 @@
 <?php
-define("USERS", ["pswsm" => ["password" => "pswsm-adm", "role" => "administrador"], "notpswsm" => ["password" => "pswsm-clt", "role" => "client"]]);
+define('FIELDS', ['password', 'role']);
+
+
+function csvToArr(): array {
+	$fh = fopen("./users.csv", 'r');
+	$kvArr = [];
+	while (!feof($fh)) {
+		$tmpArr = fgetcsv($fh);
+		$kvArr[$tmpArr[0]] = [FIELDS[0] => $tmpArr[1], FIELDS[1] => $tmpArr[2]];
+	}
+	fclose($fh);
+	return $kvArr;
+}
+
+function searchUser(string $username, string $password): string {
+	$users = csvToArr();
+	if (array_key_exists($username, $users)) {
+		if ($users[$username]["password"] == $password) {
+			$ret_value = json_encode([$username, $users[$username]["role"]]);
+		} else {
+			$ret_value = json_encode("Password does not match");
+		}
+	} else {
+		$ret_value = json_encode("Username not found");
+	};
+	return $ret_value;
+}
+
+function makeUser(string $username, string $password, string $role = 'user'): string {
+	if (!array_key_exists($username, csvToArr())) {
+		$fh = fopen("./users.csv", 'a');
+		fputcsv($fh, [$username, $password, $role]);
+		fclose($fh);
+		$retValue = json_encode([$username]);
+	} else {
+		$retValue = json_encode("User already exists.");
+	};
+	return $retValue;
+}
+
 $data = file_get_contents("php://input");
 $parsedData = json_decode($data, true);
-if (array_key_exists($parsedData["username"], USERS)) {
-	if (USERS[$parsedData["username"]]["password"] == $parsedData["password"]) {
-		$ret_value = json_encode([$parsedData["username"], USERS[$parsedData["username"]]["role"]]);
-	} else {
-		$ret_value = json_encode("Password does not match");
-	}
-} else {
-	$ret_value = json_encode("Username not found");
-};
-echo $ret_value;
+
+switch ($parsedData["operation"]) {
+	case 'login':
+		echo searchUser($parsedData["username"], $parsedData["password"]);
+		break;
+
+	case 'create':
+		echo makeUser($parsedData["username"], $parsedData["password"]);
+		break;
+	
+	default:
+		echo json_encode("Could not reach DB");
+		break;
+}
+
 ?>
